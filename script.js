6b3148cb92a0c16f5d3973d64f88d4860301e30f
ajoshi103 <100084710+ajoshi103@users.noreply.github.com>
2023-06-21 09:37:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
       
              if(firstLetter === 'j'){
-                 byeSpeaker.speak((names[i]));
+                 bye.speak((names[i]));
              }
              else {
                 helloSpeaker.speak((names[i]));
