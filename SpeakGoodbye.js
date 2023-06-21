945415f4004567203515ebcd84560e2f0fccb453
ajoshi103 <100084710+ajoshi103@users.noreply.github.com>
2023-06-21 09:37:23 +0530

    Goodbye.js

--- SpeakGoodbye.js
+++ SpeakGoodbye.js
@@ -3,11 +3,11 @@
 (function (window) {
 	var speakWord = "Good Bye";
   
-	var byeSpeaker = {};
-	byeSpeaker.speak = function (name) {
+	var bye = {};
+	bye.speak = function (name) {
 	  console.log(speakWord + " " + name);
 	};
   
-	window.byeSpeaker = byeSpeaker;
+	window.bye = bye;
   })(window);
   
